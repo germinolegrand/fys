736e6566555569f88f3d562a2f9cfc98215f9249
germinolegrand <germinolegrand@gmail.com>
2014-09-01 00:27:38 +0200

    Create find_nth.h
    An other algorithm for a future proposal

---
+++ algorithm/find_nth.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <algorithm>
+
+template<class iterator, class T>
+iterator find_nth(iterator itBegin, iterator itEnd, size_t n, T const& value)
+{
+    for(; (itBegin = std::find(itBegin, itEnd, value)) != itEnd && n != 0; ++itBegin, --n);
+
+    return itBegin;
+}
+
+template<class iterator, class F>
+iterator find_nth_if(iterator itBegin, iterator itEnd, size_t n, F f)
+{
+    for(; (itBegin = std::find_if(itBegin, itEnd, f)) != itEnd && n != 0; ++itBegin, --n);
+
+    return itBegin;
+}
