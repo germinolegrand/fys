92c7d84c3e5a6ef16abb9fca449cae7a11d17f04
germinolegrand <germinolegrand@gmail.com>
2014-09-01 03:01:22 +0200

    Create windows.h
    algorithms for future proposal

---
+++ algorithm/windows.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <algorithm>
+#include <iterator>
+
+template<class ForwardIt, class OutputIt>
+OutputIt incomplete_windows(ForwardIt itBegin, ForwardIt itEnd, OutputIt itOut, typename std::iterator_traits<ForwardIt>::difference_type windowSize, typename std::iterator_traits<ForwardIt>::difference_type windowOffset = 1)
+{
+    for(; itBegin != itEnd; std::advance(itBegin, std::min(windowOffset, std::distance(itBegin, itEnd))))
+        itOut = std::copy(itBegin, std::next(itBegin, std::min(windowSize, std::distance(itBegin, itEnd))), itOut);
+
+    return itOut;
+}
+
+template<class ForwardIt, class OutputIt>
+OutputIt complete_windows(ForwardIt itBegin, ForwardIt itEnd, OutputIt itOut, typename std::iterator_traits<ForwardIt>::difference_type windowSize, typename std::iterator_traits<ForwardIt>::difference_type windowOffset = 1)
+{
+    for(; std::distance(itBegin, itEnd) >= windowSize; std::advance(itBegin, std::min(windowOffset, std::distance(itBegin, itEnd))))
+        itOut = std::copy(itBegin, std::next(itBegin, std::min(windowSize, std::distance(itBegin, itEnd))), itOut);
+
+    return itOut;
+}
